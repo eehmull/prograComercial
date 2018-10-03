884f2701f438dcec0c0994e8778df589f7c78b02
eehmull <eehmull@gmail.com>
2018-10-04 01:45:26 +0200

    Se agregó el detalle de la publicación

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicar
 # Create your views here.
 def listar(request):
     articulos = Publicar.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar.html', {'articulos':articulos})
+
+def detalle(request, pk):
+    pub = get_object_or_404(Publicar, pk=pk)
+    return render(request, 'blog/detalle.html', {'pub': pub})
